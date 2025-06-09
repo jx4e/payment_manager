23e5db469326789a5546b548702211969f750e2b
Jake <jgaunt200@gmail.com>
2025-06-08 17:08:51 -0700

    Added spring security

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,10 +23,13 @@ dependencies {
     implementation("com.j2html:j2html:1.5.0")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6")
 
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
     "developmentOnly"("org.springframework.boot:spring-boot-devtools")
 }
 
---
+++ src/main/java/me/jx4e/paymentmanager/config/MvcConfig.java
@@ -0,0 +1,14 @@
+package me.jx4e.paymentmanager.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring6.expression.Mvc;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("/login");
+        registry.addViewController("/hello").setViewName("/hello");
+    }
+}
